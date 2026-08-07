a41deb00fe4bf44e6c4abfb12f5cf4af20158600
Simon Dutka <Szymon.dutka@outlook.com>
2026-08-07 14:29:32 +0200

    feat: add main function

--- main.py
+++ main.py
@@ -107,3 +107,19 @@ def print_report(overall, by_category, by_salesperson, top_products):
     print(
         f"{indent}{'By profit:':<{top_label_width}}{top_products['by_profit'][0]} ({top_products['by_profit'][1]:.2f})"
     )
+
+
+def main():
+    csv_file = "./data/sample_sales.csv"
+
+    transactions = get_data(csv_file)
+    overall = calculate_overall(transactions)
+    by_category = calculate_by_field(transactions, "category")
+    by_salesperson = calculate_by_field(transactions, "salesperson")
+    top_products = find_top_products(transactions)
+
+    print_report(overall, by_category, by_salesperson, top_products)
+
+
+if __name__ == "__main__":
+    main()
