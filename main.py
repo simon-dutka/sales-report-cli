aac750d5264d33a506479116257ed7290360d806
Simon Dutka <Szymon.dutka@outlook.com>
2026-08-11 19:22:19 +0200

    refactor: simplify main.py, remove direct calculation imports

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from src.calculations import calculate_by_field, calculate_overall, find_top_products
-from src.data_loader import get_data
-from src.report import print_report
+from src.interface import main_menu
 
 
 def main():
-    csv_file = "./data/sample_sales.csv"
+    loaded_csv_file = None
 
-    transactions = get_data(csv_file)
-    overall = calculate_overall(transactions)
-    by_category = calculate_by_field(transactions, "category")
-    by_salesperson = calculate_by_field(transactions, "salesperson")
-    top_products = find_top_products(transactions)
-
-    print_report(overall, by_category, by_salesperson, top_products)
+    while True:
+        loaded_csv_file = main_menu(loaded_csv_file)
 
 
 if __name__ == "__main__":
